d9749786a886adb95b5865f636fdb9e4b989d87f
Camden Van Sciver <camdenvs02@gmail.com>
2025-04-01 20:03:27 -0700

    open new tab on external links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,8 +10,8 @@ const Footer = () => {
             <Box display={'flex'} height={'100px'} justifyContent={'space-evenly'} marginTop={'25px'}>
                 <Text fontSize={'20px'} fontWeight={'600'} alignSelf={'center'}>©Flintlox LLC</Text>
                 <Box display={'flex'} alignSelf={'center'}>
-                    <Link href='https://www.instagram.com/flintloxleathergoods/'><FaInstagram size={'50px'} /></Link>
-                    <Link href='https://twitter.com/flintloxleather'><Image src="/images/x-twitter.svg" marginX='20px' width={'50px'}/></Link>
+                    <Link href='https://www.instagram.com/flintloxleathergoods/' target='_blank' rel="noopener noreferrer"><FaInstagram size={'50px'} /></Link>
+                    <Link href='https://twitter.com/flintloxleather' target='_blank' rel="noopener noreferrer"><Image src="/images/x-twitter.svg" marginX='20px' width={'50px'}/></Link>
                     <Link href='mailto:admin@flintlox.com'><FaEnvelope size={'50px'}/></Link>
                 </Box>
             </Box>
